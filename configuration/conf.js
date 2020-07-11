0fa1922318d8a6fdc5462bcba71949be4abfbef9
Artur Strug <artstrug@gmail.com>
2020-07-12 00:04:38 +0200

    Add html screenshot reporter to conf.js

--- configuration/conf.js
+++ configuration/conf.js
@@ -1,3 +1,10 @@
+var HtmlScreenshotReporter = require('protractor-jasmine2-screenshot-reporter');
+
+var reporter = new HtmlScreenshotReporter({
+  dest: 'reports/screenshots_and_reports',
+  filename: 'test_report.html'
+});
+
 exports.config = {
     //seleniumAddress: 'http://localhost:4444/wd/hub',
     directConnect: true,
@@ -8,10 +15,27 @@ exports.config = {
     },
     suites: {
       login_tests: '../tests/login_spec.js'      
+    },    
+    framework: 'jasmine',
+
+    // Setup the report before any tests start
+    beforeLaunch: function() {
+      return new Promise(function(resolve){
+        reporter.beforeLaunch(resolve);
+      });
     },
-    onPrepare: function () {
-        browser.manage().window().maximize();
-        browser.manage().timeouts().implicitlyWait(5000);
+
+    // Assign the test reporter to each running instance
+    onPrepare: function() {
+      jasmine.getEnv().addReporter(reporter);
+      browser.manage().window().maximize();
+      browser.manage().timeouts().implicitlyWait(5000);
     },
-    framework: 'jasmine'
-  };
+
+    // Close the report after all tests finish
+    afterLaunch: function(exitCode) {
+      return new Promise(function(resolve){
+        reporter.afterLaunch(resolve.bind(this, exitCode));
+      });
+    }
+  }
